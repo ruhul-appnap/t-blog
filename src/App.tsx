f6e7656c8dbd24ff6a475b41e5acbb4cd8ee90e2
Ruhul <ruhulinfo50@gmail.com>
2024-01-07 22:47:55 +0600

    feat: Add button component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import { Button } from './components'
+
+function App() {
+
+  return (
+    <>
+      <Button variant='primary' className='width-100'>submit</Button>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type ButtonProps = {
+  variant: "primary" | "secondary"
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+
+const buttonVariant = {
+  primary: "button-base primary-button",
+  secondary: "button-base secondary-button"
+}
+
+function Button(props: ButtonProps) {
+  const { children, className, variant } = props;
+
+  let buttonType = buttonVariant[variant] ? buttonVariant[variant] : "button-base"
+  if (className) buttonType += ' ' + className
+
+  return (
+    <button {...props} className={`${buttonType}`}>{children}</button>
+  )
+}
+
+export default Button
